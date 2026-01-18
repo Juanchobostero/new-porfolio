3a8c3fd4df6ef7fde14c52fef1bde11170544b2b
JUANCHO <juancruzmart93@gmail.com>
2026-01-18 16:16:13 -0300

    Modificaciones Enero 2026 v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://juanchodev.com',
+  integrations: [tailwind()],
+  sitemap: true
 });
